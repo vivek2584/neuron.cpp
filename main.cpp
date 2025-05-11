a9ddfa1358ac27f371f1fd41a0e3c261682cd484
vivek <vivekroy2584@gmail.com>
2025-05-11 22:06:06 +0000

    neural net weights matrices constructors

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-#include "matrix.hpp"
+#include "neuron.hpp"
 
 int main(){
-  /*Matrix<2, 3> A;
-    Matrix<3, 2> B;
+    Network<2, 3, 2> net{0.01f};
 
-    A.m_matrix = {{
-        {{1.0, 2.0, 3.0}},
-        {{4.0, 5.0, 6.0}}
-    }};
-
-    B.m_matrix = {{
-        {{7.0, 8.0}},
-        {{9.0, 10.0}},
-        {{11.0, 12.0}}
-    }};
-
-    Matrix<2, 2> C = A * B;
-
-    for (size_t i = 0; i < 2; ++i) {
-        for (size_t j = 0; j < 2; ++j) {
-            std::cout << C.m_matrix[i][j] << " ";
+    for(size_t i = 0; i < 3 ; i++){
+        for(size_t j = 0; j < 2; j++){
+            std::cout << net.hidden_weights.m_matrix[i][j] << " ";
         }
-        std::cout << '\n';
+        std::cout << "\n";
     }
 
-    return 0;*/
+    for(size_t i = 0; i < 2 ; i++){
+        for(size_t j = 0; j < 3; j++){
+            std::cout << net.output_weights.m_matrix[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
 }
--- neuron.hpp
+++ neuron.hpp
@@ -1,10 +1,37 @@
 #ifndef NEURON_HPP
 #define NEURON_HPP
 
+#include <random>
+#include <cmath>
 #include "matrix.hpp"
 
-class Network{
-    
+template<size_t inputs, size_t hiddens, size_t outputs>
+struct Network{
+
+    Matrix<hiddens, inputs> hidden_weights;
+    Matrix<outputs, hiddens> output_weights;
+    float learning_rate;
+
+    Network(float rate) : learning_rate(rate){
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        float min = -1/(std::sqrt(static_cast<float>(inputs)));
+        float max = +1/(std::sqrt(static_cast<float>(inputs)));
+        std::uniform_real_distribution<float> distr(min, max);
+        
+        for(size_t i = 0; i < hiddens ; i++){
+            for(size_t j = 0; j < inputs; j++){
+                hidden_weights.m_matrix[i][j] = distr(gen);
+            }
+        }
+
+        for(size_t i = 0; i < outputs ; i++){
+            for(size_t j = 0; j < hiddens; j++){
+                output_weights.m_matrix[i][j] = distr(gen);
+            }
+        }
+    }
+
 };
 
 
